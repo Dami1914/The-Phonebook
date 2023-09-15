1c9cf2ee16596dae413fde32184a2e3436f75248
Damilola Jibowu <Aesthetico.design@gmail.com>
2023-09-15 21:43:15 +0100

    part2 2.9* phonebook implemented a search field for searching for a person in a contact list by name or number

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useState } from 'react'
 function App() {
 
   const [contactName, setContactName] = useState([])
+  const [contactFilter, setContactFilter] = useState("")
   const [newContact, setNewContact] = useState({
     name: "",
     number:""
   })
 
+//handles input for name and number
   function handleChange(event){
     const {value,id} = event.target
     setNewContact(prev=>{
@@ -16,48 +18,87 @@ function App() {
     console.log(newContact)
   }
 
+  //handle operations after submit button is cliked like saving the inputed data,checking if name or contact existed before and so on
   function handleSubmit(event){
     event.preventDefault()
-    handleContactCheck() == true ? alert(`${newContact.name} already exist`): setContactName(prev=>{
-      return prev?.concat([newContact])
+    const {name,number} = newContact
+    const {nameStatus,numberStatus} = handleContactCheck()
+    console.log(nameStatus,numberStatus)
+    nameStatus||numberStatus ? alert(`${nameStatus?nameStatus:numberStatus} already exist`): setContactName(prev=>{
+      return prev?.concat([{name:capitalizeFirstChar(name.toLowerCase()),number:number}])
     });
     
     setNewContact({name:"",number:""})
   }
 
+  //capitalizes the first character of inputed strings
+  function capitalizeFirstChar(str){
+    // text.split(" ") turns strings into an array also considering the space between them (" ") e.g input "damilola jibowu" output ["damilola", "jibowu"] 
+    return str.split(" ").map((ele)=>{
+      return ele.charAt(0).toUpperCase() + ele.slice(1)
+      // join(" ") turns content of an array to single a string having also considering the space in between them (" ")
+    }).join(" ")
+  }
+  
+  //this receives filter value for filtering the displayed contact
+  function handleFilter(event){
+    const {value} = event.target
+    setContactFilter(value)
+  }
+
+// checking if contact contains inputed name or number
   function handleContactCheck(){
-    let truthstate = ""
+    let returnedvalue = {
+      nameStatus: "",
+      numberStatus:""
+    }
+    //checks if name exist before in the contact list
       contactName.forEach((ele)=>{
-          if(ele.name.trim() === newContact.name.trim() || ele.number ===  newContact.number ){
-            truthstate = true
+          if(ele.name.trim().toLowerCase() === newContact.name.trim().toLowerCase()){
+            returnedvalue.nameStatus = newContact.name
           }else {
-            truthstate = false
+            returnedvalue.nameStatus = false
           }
       })
+      //checks if number exist before in the contact list
+      contactName.forEach((ele)=>{
+        ele.number.trim() === newContact.number.trim() ? returnedvalue.numberStatus = newContact.number : returnedvalue.numberStatus = false
+      })
 
-      return truthstate
+      return returnedvalue
   }
+
   return (
     <>
        <div>
           <h1>Phonebook</h1>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <div>
-                <input onChange={handleChange} id="name" value={newContact.name} type="text" placeholder="Add name"/> 
-              </div>
-              <div>
-                <input onChange={handleChange} id="number" value={newContact.number} type="tel" placeholder="Add number"/>
-              </div>
-              <button type="submit">add</button>
-            </div>
+          <form> 
+              <input type="search" onChange={handleFilter} value={contactFilter} placeholder="filter shown with"/>
           </form>
+          <div>
+            <h1>add a new</h1>
+            <form onSubmit={handleSubmit}>
+              <div>
+               <div>
+                 <input onChange={handleChange} id="name" aria-required value={newContact.name} type="text" placeholder="Add name"/> 
+               </div>
+               <div>
+                 <input onChange={handleChange} id="number" aria-required value={newContact.number} type="tel" placeholder="Add number"/>
+               </div>
+               <button type="submit">add</button>
+              </div>
+            </form>
+          </div>
        </div>
        <div>
           <h1>Numbers</h1>
           <ul>
-            {contactName.map((elem)=>{
-              return <li key={elem.number}>{elem.name} {elem.number} </li>
+            {
+            // filters the contactName based on some conditions and the maps the returned array for filtered render of data
+            contactName.filter((elem)=>{
+             return elem.name.toLowerCase().includes(contactFilter.toLowerCase()) || elem.number.includes(contactFilter)
+            }).map((ele)=>{
+                return <li key={ele.number}>{ele.name} {ele.number}</li>
             })}
           </ul>
        </div>
