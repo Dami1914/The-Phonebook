934aaac78e0ffc86e5375e0f0888302d78cf3e89
Damilola Jibowu <Aesthetico.design@gmail.com>
2023-09-15 14:37:22 +0100

    part2 2.8 phonebook added input field for adding number to the contact list

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,32 @@ import { useState } from 'react'
 function App() {
 
   const [contactName, setContactName] = useState([])
-  const [newName, setNewName] = useState('')
+  const [newContact, setNewContact] = useState({
+    name: "",
+    number:""
+  })
 
   function handleChange(event){
-    const {value} = event.target
-    setNewName(value)
-    console.log(newName)
+    const {value,id} = event.target
+    setNewContact(prev=>{
+      return {...prev,[id]:value}
+    })
+    console.log(newContact)
   }
 
   function handleSubmit(event){
     event.preventDefault()
-    handleContactCheck() == true ? alert(`${newName} already exist`): setContactName(prev=>{
-      return prev?.concat([{name:newName}])
+    handleContactCheck() == true ? alert(`${newContact.name} already exist`): setContactName(prev=>{
+      return prev?.concat([newContact])
     });
-
-    setNewName('')
+    
+    setNewContact({name:"",number:""})
   }
 
   function handleContactCheck(){
     let truthstate = ""
       contactName.forEach((ele)=>{
-          if(ele.name.trim() === newName.trim()){
+          if(ele.name.trim() === newContact.name.trim() || ele.number ===  newContact.number ){
             truthstate = true
           }else {
             truthstate = false
@@ -37,19 +42,26 @@ function App() {
        <div>
           <h1>Phonebook</h1>
           <form onSubmit={handleSubmit}>
-              <input onChange={handleChange} value={newName} type="text" placeholder="Add name"/>
+            <div>
+              <div>
+                <input onChange={handleChange} id="name" value={newContact.name} type="text" placeholder="Add name"/> 
+              </div>
+              <div>
+                <input onChange={handleChange} id="number" value={newContact.number} type="tel" placeholder="Add number"/>
+              </div>
               <button type="submit">add</button>
+            </div>
           </form>
        </div>
        <div>
           <h1>Numbers</h1>
           <ul>
             {contactName.map((elem)=>{
-              return <li key={elem.name}>{elem.name}</li>
+              return <li key={elem.number}>{elem.name} {elem.number} </li>
             })}
           </ul>
        </div>
-       <div>debug: {newName}</div>
+       <div>debug: {newContact.name}</div>
     </>
   )
 }
